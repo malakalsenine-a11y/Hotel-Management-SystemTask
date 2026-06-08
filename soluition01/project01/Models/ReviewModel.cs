3087a9ddc9641a7f7ec5e768d79097f09d2959f6
Malakaka <malakalsenine@gmail.com>
2026-06-08 12:46:21 +0400

    Create the ReviewModel class.

--- soluition01/project01/Models/ReviewModel.cs
+++ soluition01/project01/Models/ReviewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,14 @@ namespace project01.Models
 {
     public class ReviewModel
     {
+        public string reviewId { get; set; }
+        public string bookingId { get; set; }
+        public int rating  { get; set; }
+        public string comment { get; set; }
+
+
+
+
+
     }
 }
