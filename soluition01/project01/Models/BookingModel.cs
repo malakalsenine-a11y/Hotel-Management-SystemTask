98804cce8a620f0bae27a444206c99ae8aa369bb
Malakaka <malakalsenine@gmail.com>
2026-06-08 12:41:24 +0400

    Create the BookingModel class.

--- soluition01/project01/Models/BookingModel.cs
+++ soluition01/project01/Models/BookingModel.cs
@@ -8,5 +8,16 @@ namespace project01.Models
 {
     public class BookingModel
     {
+        public string bookingId { get; set; }
+        public string guestId { get; set; }
+        public string roomNumber { get; set; }
+        public string checkInDate { get; set; }
+        public string checkOutDate { get; set; }
+        public double totalPrice { get; set; }
+        public string status { get; set; }
+
+        //public List <ReviewModel> review { get; set; }
+
+
     }
 }
